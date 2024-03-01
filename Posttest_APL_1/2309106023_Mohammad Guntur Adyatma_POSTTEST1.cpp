0169e68c529fbad2910916c1fab793b0e36b32c3
GalaxionZero <zetayu24@gmail.com>
2024-03-01 20:55:30 +0800

    submitting posttest 1 files

---
+++ Posttest_APL_1/2309106023_Mohammad Guntur Adyatma_POSTTEST1.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+using namespace std;
+
+
+bool programIsFinished = false;
+
+struct
+{
+    double rupiah = 0.00;
+    double dollar = 0.00;
+    double euro = 0.00;
+    double yen = 0.00;
+} dataCurrency;
+
+struct
+{
+    string myName = "Guntur";
+    string myNIM = "2309106023";
+} dataMahasiswa;
+
+
+void currencyExchange(int whichCurrency, double insertedCurrency)
+{
+    switch(whichCurrency)
+    {
+    case 1:
+        dataCurrency.rupiah = insertedCurrency;
+        dataCurrency.dollar = insertedCurrency / 15000;
+        dataCurrency.euro = insertedCurrency / 17000;
+        dataCurrency.yen = insertedCurrency / 104;
+        break;
+    case 2:
+        dataCurrency.rupiah = insertedCurrency * 15000;
+        dataCurrency.dollar = insertedCurrency;
+        dataCurrency.euro = insertedCurrency * 0.92;
+        dataCurrency.yen = insertedCurrency * 150;
+        break;
+    case 3:
+        dataCurrency.rupiah = insertedCurrency * 17000;
+        dataCurrency.dollar = insertedCurrency * 1.08;
+        dataCurrency.euro = insertedCurrency;
+        dataCurrency.yen = insertedCurrency * 162;
+        break;
+    case 4:
+        dataCurrency.rupiah = insertedCurrency * 104;
+        dataCurrency.dollar = insertedCurrency / 150;
+        dataCurrency.euro = insertedCurrency / 162;
+        dataCurrency.yen = insertedCurrency;
+        break;
+    }
+
+}
+
+
+int menu(int numberOfChoices)
+{
+    while (true)
+    {
+        int choice;
+        cin >> choice;
+        if (choice <= numberOfChoices)
+        {
+            return choice;
+        }
+            else
+            {
+                cout << "Masukkan sesuai jumlah opsi!";
+            }
+    }
+}
+
+
+void currencyExchangeMain()
+{
+    bool programRepeats = true;
+    int currencyChoice, repeatChoice;
+    double amountOfCurrency;
+
+    cout << endl << "Welcome to Currency Exchange Limited! \n";
+
+    while (programRepeats == true)
+    {
+        cout << endl << "Choose a currency to exchange from: \n"
+                "1. Rupiah \n"
+                "2. Dollar \n"
+                "3. Euro \n"
+                "4. Yen \n";
+
+        currencyChoice = menu(4);
+        cout << endl << "Enter the desired amount to be converted: \n";
+        cin >> amountOfCurrency;
+
+        currencyExchange(currencyChoice, amountOfCurrency);
+
+        cout << endl << "The currency after being converted: \n";
+        cout << "Rupiah: " << dataCurrency.rupiah << endl;
+        cout << "Dollar: " << dataCurrency.dollar << endl;
+        cout << "Euro: " << dataCurrency.euro << endl;
+        cout << "Yen: " << dataCurrency.yen << endl;
+
+
+        cout << endl << "Would you like to repeat the program? \n"
+                        "1. Yes \n"
+                        "2. No \n";
+        repeatChoice = menu(2);
+        if (repeatChoice = 2)
+        {
+            programRepeats = false;
+            programIsFinished = true;
+        }
+        cout << endl;
+    }
+}
+
+
+int loginCheck(string insertedName, string insertedNIM)
+{
+    if (insertedName == dataMahasiswa.myName && insertedNIM == dataMahasiswa.myNIM)
+    {
+        return true;
+    }
+        else
+        {
+            return false;
+        }
+}
+
+int main()
+{
+    string inputName, inputNIM;
+    int loginAttempts = 0;
+    bool tryLogin = false;
+
+    while (loginAttempts < 3 && programIsFinished == false)
+    {
+        cout << "Enter your username: ";
+        cin >> inputName;
+        cout << "Enter your NIM: ";
+        cin >> inputNIM;
+
+        tryLogin = loginCheck(inputName, inputNIM);
+
+        if (tryLogin == false)
+            {
+            cout << "Login Failed! \n"
+                    "Trying again... \n";
+            loginAttempts += 1;
+        }
+
+            else
+                {
+                cout << "Login Succeeded! \n"
+                        "Entering Program \n";
+                currencyExchangeMain();
+            }
+
+    }
+
+    return 0;
+}
